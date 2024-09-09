2db282ddfb63fdf4ddf37f79bae90eb1ac0e290d
seandeng2019 <xiang1022@gmail.com>
2024-09-09 13:13:26 +0800

    Site updated: 2024-09-09 13:13:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/08/hello-world/"];function toRandomPost(){
+var posts=["2024/09/08/hello-world/","2024/09/09/我的第一篇blog/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
