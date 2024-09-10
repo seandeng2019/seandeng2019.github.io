d9e07ec4434d050f189e549b65fa02d7c5fc9a86
seandeng2019 <xiang1022@gmail.com>
2024-09-10 08:11:53 +0800

    Site updated: 2024-09-10 08:11:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/09/这是一篇新的博文/","2024/09/08/hello-world/"];function toRandomPost(){
+var posts=["2024/09/09/免费虚拟主机/","2024/09/09/这是一篇新的博文/","2024/09/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
